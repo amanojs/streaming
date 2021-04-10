62611ff559ef336624aa3f592aaf106ff4d1d474
tanabe1422 <1422hal@gmail.com>
2021-04-10 22:39:49 +0900

    feat(CopyRoomIdButton): 機能部分完成 クリックしたらURLがコピーされて通知が出ます。

--- src/components/CopyRoomIdButton/index.tsx
+++ src/components/CopyRoomIdButton/index.tsx
@@ -1,9 +1,29 @@
-import { Button } from '@material-ui/core';
 import * as React from 'react';
+import { useSnackbar } from 'notistack';
+import { Presenter } from './Presenter';
 
+/**
+ * URLをクリップボードにコピーするボタン
+ */
 export const CopyRoomIdButton: React.FC = () => {
-  const onClick = () => {
-    console.log('押したよ！');
+  const [url] = React.useState<string>(window.location.href);
+  const urlInputRef = React.useRef<HTMLInputElement>(null);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const onClick = (): void => {
+    // 選択
+    console.log(!!urlInputRef.current);
+    urlInputRef.current?.select();
+
+    // クリップボードにコピー
+    document.execCommand('copy');
+
+    // 通知
+    enqueueSnackbar('招待URLをコピーしました。', {
+      variant: 'info',
+      anchorOrigin: { horizontal: 'center', vertical: 'top' },
+      autoHideDuration: 2000
+    });
   };
-  return <Button {...{ onClick: onClick }}>Copy URL</Button>;
+  return <Presenter url={url} inputRef={urlInputRef} onClick={onClick} />;
 };
