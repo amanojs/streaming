500d7d81e84a51c653c2b851344c783605a4c1b5
takashiVue <takashivue@gmail.com>
2021-04-12 10:30:50 +0900

    feat(env.base,PlayingBoard,Wrap): 再生中の動画タイトルをアニメーションで表示するコンポーネントを作成
    再生中の動画タイトルを videoId を元に YouTube Data API から取得し、アニメーションと共に表示する機能、スタイルングを実装。
    
    BREAKING CHANGE: YouTubeDataAPIを利用しているので.env.baseを参考に.envファイルを各自編集してください。

---
+++ src/components/PlayingBoard/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import axios from 'axios';
+import { Presenter } from './Presenter';
+import './main.css';
+
+interface PlayingBoardProps {
+  videoId: string;
+}
+
+export const PlayingBoard: React.FC<PlayingBoardProps> = (props: PlayingBoardProps) => {
+  const [title, setTitle] = React.useState('');
+  const APIKEY: string = process.env.REACT_APP_YOUTUBE_KEY as string;
+  React.useEffect(() => {
+    if (!props.videoId) return;
+    getTitle();
+  }, [props.videoId]);
+  const getTitle = () => {
+    axios
+      .get(`https://www.googleapis.com/youtube/v3/search?key=${APIKEY}&part=id,snippet`, {
+        params: { q: props.videoId }
+      })
+      .then(({ data, status }) => {
+        if (status === 200) {
+          const title = data.items[0].snippet.title;
+          setTitle(title);
+        }
+      });
+  };
+  return <Presenter title={title} />;
+};
