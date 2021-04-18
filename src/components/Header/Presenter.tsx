95201bbde31f4e5299ade4efd9ee2161ee7b6a84
takashiVue <takashivue@gmail.com>
2021-04-19 04:24:26 +0900

    fix(Header,Home): スマホのレイアウトを考え、ロゴの高さを明示

--- src/components/Header/Presenter.tsx
+++ src/components/Header/Presenter.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import { CopyRoomIdButton } from '../CopyRoomIdButton';
 import { ArrowLeftRounded } from '@material-ui/icons';
-import Logo from '../../assets/Streaming-logo.png';
+import Logo2 from '../../assets/Streaming-logo2.png';
 import './main.css';
 
 interface PresenterProps {
@@ -17,8 +17,8 @@ export const Presenter: React.FC<PresenterProps> = (props: PresenterProps) => {
           <IconButton size="small" edge="start" color="default" aria-label="menu" onClick={props.backClick}>
             <ArrowLeftRounded fontSize="large" />
           </IconButton>
-          <h2 style={{ flexGrow: 1, display: 'flex' }} className="header_logo">
-            <img src={Logo} width="25px" style={{ marginRight: '5px' }} />
+          <h2 style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }} className="header_logo">
+            <img src={Logo2} width="25px" height="25px" style={{ marginRight: '5px' }} />
             <span>S</span>treaming!!
           </h2>
           <CopyRoomIdButton />
