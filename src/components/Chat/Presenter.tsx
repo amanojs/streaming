33eb3fb1df350068d23a31e90a780b97c7e4f7e6
amanojs <takashivue@gmail.com>
2021-04-24 20:51:42 +0900

    feat(Chat,Room): チャットコンポーネントレイアウトの配置、レスポンシブを設定
    PCだと動画の横にチャット欄がくるように、タブレットorスマホだと動画の下にチャット欄が来るように配置、レスポンシブの設定をしました。

---
+++ src/components/Chat/Presenter.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Grid } from '@material-ui/core';
+import './main.css';
+
+export const Presenter: React.FC = () => {
+  return (
+    <Grid container className="chatBase">
+      <Grid item xs={12}>
+        aaa
+      </Grid>
+    </Grid>
+  );
+};
