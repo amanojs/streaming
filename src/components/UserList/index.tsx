cf75198f09e1a83de290600943641569735372cd
tanabe1422 <1422hal@gmail.com>
2021-04-26 01:53:07 +0900

    デザインの調整

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,10 +1,21 @@
-import { IconButton, Menu, MenuItem, Dialog } from '@material-ui/core';
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Dialog,
+  List,
+  ListItemText,
+  ListItem,
+  DialogTitle,
+  ListItemAvatar,
+  Avatar
+} from '@material-ui/core';
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { User } from '../../store/modules/roomModule';
 import { State } from '../../store/store';
 
-import { Person } from '@material-ui/icons';
+import { Person, Close } from '@material-ui/icons';
 
 export const UserList: React.FC = () => {
   const userList: User[] = useSelector((state: State) => state.room.userList);
@@ -36,6 +47,25 @@ export const UserList: React.FC = () => {
     setAnchorEl(null);
   };
 
+  const userListBody = (): JSX.Element => {
+    return (
+      <List style={{ overflowY: 'scroll' }}>
+        {userList.map((user) => {
+          return (
+            <ListItem key={user.id}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Person />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={user.name} />
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
+
   return (
     <div>
       <IconButton size="small" color="secondary" onClick={handleClick}>
@@ -48,24 +78,32 @@ export const UserList: React.FC = () => {
         keepMounted={true}
         open={Boolean(anchorEl) && !isSmallScreen()}
         onClose={handleClose}
+        style={{ overflowY: 'hidden' }}
       >
-        {userList.map((user) => {
-          return <MenuItem key={user.id}>{user.name}</MenuItem>;
-        })}
+        <DialogTitle id="simple-dialog-title">
+          <span>User List</span>
+          <IconButton onClick={handleClose} color="secondary" aria-label="add to shopping cart">
+            <Close />
+          </IconButton>
+        </DialogTitle>
+        {userListBody()}
       </Menu>
       <Dialog
         className="userList_modal"
         style={{ position: 'absolute' }}
         open={Boolean(anchorEl) && isSmallScreen()}
         onClose={handleClose}
+        fullScreen
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        <div>
-          {userList.map((user) => {
-            return <div key={user.id}>{user.name}</div>;
-          })}
-        </div>
+        <DialogTitle id="simple-dialog-title">
+          <span>User List</span>
+          <IconButton onClick={handleClose} color="secondary" aria-label="add to shopping cart">
+            <Close />
+          </IconButton>
+        </DialogTitle>
+        {userListBody()}
       </Dialog>
     </div>
   );
